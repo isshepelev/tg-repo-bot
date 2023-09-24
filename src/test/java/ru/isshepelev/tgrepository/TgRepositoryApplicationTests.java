3133de00f2a131e4ea9a48377614998baeec1f89
Ilya <isshepelev2004@gmail.com>
2023-09-24 22:19:18 +0300

    first commit

---
+++ src/test/java/ru/isshepelev/tgrepository/TgRepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.isshepelev.tgrepository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgRepositoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
