fde8a4ac7de87bf1d76e93cee4a4e5ede6f56c66
Ilya <isshepelev2004@gmail.com>
2023-09-25 22:20:39 +0300

    add repository car-salon-api

--- src/main/java/ru/isshepelev/tgrepository/bot/TelegramBot.java
+++ src/main/java/ru/isshepelev/tgrepository/bot/TelegramBot.java
@@ -32,11 +32,18 @@ public class TelegramBot extends TelegramLongPollingBot {
     public TelegramBot(UserRepository userRepository) {
         this.userRepository = userRepository;
         this.botConfig = new BotConfig();
-        List<BotCommand> listCommands = new ArrayList();
+        List<BotCommand> listCommands = new ArrayList<>();
         listCommands.add(new BotCommand("/start", "start bot"));
         listCommands.add(new BotCommand("/info", "information about you"));
         listCommands.add(new BotCommand("/project", "my repository"));
         listCommands.add(new BotCommand("/git", "my git repository (link)"));
+
+        listCommands.add(new BotCommand("/car_salon_api", "car-salon-api"));
+        listCommands.add(new BotCommand("/api_traffic_detector", "api-traffic-detector"));
+        listCommands.add(new BotCommand("/user_bank_api", "user-bank-api"));
+        listCommands.add(new BotCommand("/jwt_security", "jwt-security"));
+        listCommands.add(new BotCommand("/tg_repo_bot", "tg-repo-bot"));
+        listCommands.add(new BotCommand("/rest_security", "rest-security"));
 
 
         try {
@@ -80,6 +87,12 @@ public class TelegramBot extends TelegramLongPollingBot {
                 case "/help":
                     helpMessage(update.getMessage().getChatId());
                     break;
+                case "/project":
+                    mainRepositoryMessage(chatId);
+                    break;
+                case "/car_salon_api":
+                    carSalonApi(chatId);
+                    break;
                 default:
                     sendMessage(chatId, "Извините, неизветсная команда!");
                     break;
@@ -91,11 +104,29 @@ public class TelegramBot extends TelegramLongPollingBot {
             if (callBackData.equals("BACK_FOR_MAIN_MESSAGE")) {
                 mainMessage(chatId);
             }
+            if (callBackData.equals("BACK_FOR_REPO_MESSAGE")) {
+                mainRepositoryMessage(chatId);
+            }
             if (callBackData.equals("ACCOUNT_BUTTON")) {
                 infoUser(chatId);
             }
-            if (callBackData.equals("HELP_BUTTON")){
+            if (callBackData.equals("HELP_BUTTON")) {
                 helpMessage(chatId);
+            }
+            if (callBackData.equals("PROJECT_BUTTON")) {
+                mainRepositoryMessage(chatId);
+            }
+            if (callBackData.equals("CAR_SALON_API_BUTTON")) {
+                carSalonApi(chatId);
+            }
+            if (callBackData.equals("SSH_CAR_SALON_API")){
+                sshCarSalonApi(chatId);
+            }
+            if (callBackData.equals("HTTPS_CAR_SALON_API")){
+                httpCarSalonApi(chatId);
+            }
+            if (callBackData.equals("INFO_CAR_SALON_API")){
+                infoCarSalonApi(chatId);
             }
 
 
@@ -238,7 +269,7 @@ public class TelegramBot extends TelegramLongPollingBot {
         }
     }
 
-    private void helpMessage(long chatId){
+    private void helpMessage(long chatId) {
         String answer = "Добро пожаловать в помощь! Вы можете использовать следующие команды:\n" +
                 "\n" +
                 "\uD83D\uDC64 /info - Просмотр информации о вашем профиле.\n" +
@@ -269,20 +300,202 @@ public class TelegramBot extends TelegramLongPollingBot {
         }
     }
 
-    public void mainRepositoryMessage(long chatId){
+    public void mainRepositoryMessage(long chatId) {
         String answer = "\uD83D\uDCC2 Выберите один из репозиториев:";
         SendMessage sendMessage = new SendMessage();
         sendMessage.setChatId(String.valueOf(chatId));
         sendMessage.setText(answer);
 
         InlineKeyboardMarkup markup = new InlineKeyboardMarkup();
-        List<List<InlineKeyboardButton>> rowsLine1 = new ArrayList<>();
+        List<List<InlineKeyboardButton>> rowsLine = new ArrayList<>();
         List<InlineKeyboardButton> rowLine1 = new ArrayList<>();
-
-        List<List<InlineKeyboardButton>> rowsLine2 = new ArrayList<>();
         List<InlineKeyboardButton> rowLine2 = new ArrayList<>();
+        List<InlineKeyboardButton> rowBack = new ArrayList<>();
 
-        List<List<InlineKeyboardButton>> rowsLine3 = new ArrayList<>();
-        List<InlineKeyboardButton> rowLine3 = new ArrayList<>();
+        var back = new InlineKeyboardButton();
+        back.setText("Назад!");
+        back.setCallbackData("BACK_FOR_MAIN_MESSAGE");
+
+        var carSalonApi = new InlineKeyboardButton();
+        carSalonApi.setText("car-salon-api");
+        carSalonApi.setCallbackData("CAR_SALON_API_BUTTON");
+
+        var apiTrafficDetector = new InlineKeyboardButton();
+        apiTrafficDetector.setText("api-traffic-detector");
+        apiTrafficDetector.setCallbackData("API_TRAFFIC_DETECTOR_BUTTON");
+
+        var userBankApi = new InlineKeyboardButton();
+        userBankApi.setText("user-bank-api");
+        userBankApi.setCallbackData("USER_BANK_API_BUTTON");
+
+        var jwtSecurity = new InlineKeyboardButton();
+        jwtSecurity.setText("jwt-security");
+        jwtSecurity.setCallbackData("JWT_SECURITY_BUTTON");
+
+        var tgRepoBot = new InlineKeyboardButton();
+        tgRepoBot.setText("tg-repo-bot");
+        tgRepoBot.setCallbackData("TG_REPO_BOT_BUTTON");
+
+        var restSecurity = new InlineKeyboardButton();
+        restSecurity.setText("rest-security");
+        restSecurity.setCallbackData("REST_SECURITY_BUTTON");
+
+        rowLine1.add(carSalonApi);
+        rowLine1.add(apiTrafficDetector);
+        rowLine1.add(userBankApi);
+
+        rowLine2.add(jwtSecurity);
+        rowLine2.add(tgRepoBot);
+        rowLine2.add(restSecurity);
+
+        rowBack.add(back);
+
+        rowsLine.add(rowLine1);
+        rowsLine.add(rowLine2);
+        rowsLine.add(rowBack);
+
+        markup.setKeyboard(rowsLine);
+        sendMessage.setReplyMarkup(markup);
+
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            log.error("error " + e.getMessage());
+        }
+    }
+
+    public void carSalonApi(long chatId) {
+        SendMessage sendMessage = new SendMessage();
+        String text = "\uD83D\uDE97 Вы выбрали репозиторий \"car-salon-api\"!";
+        sendMessage.setText(text);
+        sendMessage.setChatId(String.valueOf(chatId));
+
+        InlineKeyboardMarkup markup = new InlineKeyboardMarkup();
+        List<List<InlineKeyboardButton>> rowsLine = new ArrayList<>();
+        List<InlineKeyboardButton> rowLine1 = new ArrayList<>();
+        List<InlineKeyboardButton> rowLine2 = new ArrayList<>();
+        List<InlineKeyboardButton> rowBack = new ArrayList<>();
+
+        var back = new InlineKeyboardButton();
+        back.setText("Назад!");
+        back.setCallbackData("BACK_FOR_REPO_MESSAGE");
+
+        var ssh = new InlineKeyboardButton();
+        ssh.setText("ssh");
+        ssh.setCallbackData("SSH_CAR_SALON_API");
+
+        var https = new InlineKeyboardButton();
+        https.setText("https");
+        https.setCallbackData("HTTPS_CAR_SALON_API");
+
+        var info = new InlineKeyboardButton();
+        info.setText("Узнать больше");
+        info.setCallbackData("INFO_CAR_SALON_API");
+
+        var gitRepo = new InlineKeyboardButton();
+        gitRepo.setText("Перейти в репозиторий");
+        gitRepo.setCallbackData("GIT_CAR_SALON_API");
+        gitRepo.setUrl("https://github.com/isshepelev/car-salon-api");
+
+        rowLine1.add(ssh);
+        rowLine1.add(https);
+
+        rowLine2.add(info);
+        rowLine2.add(gitRepo);
+
+        rowBack.add(back);
+
+        rowsLine.add(rowLine1);
+        rowsLine.add(rowLine2);
+        rowsLine.add(rowBack);
+
+        markup.setKeyboard(rowsLine);
+        sendMessage.setReplyMarkup(markup);
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            log.error("error " + e.getMessage());
+        }
+    }
+    public void sshCarSalonApi(long chatId){
+        SendMessage sendMessage = new SendMessage();
+        String text = "ssh: \n" + "git@github.com:isshepelev/car-salon-api.git";
+        sendMessage.setText(text);
+        sendMessage.setChatId(String.valueOf(chatId));
+
+        InlineKeyboardMarkup markup = new InlineKeyboardMarkup();
+        List<List<InlineKeyboardButton>> rowsLine = new ArrayList<>();
+        List<InlineKeyboardButton> rowLine = new ArrayList<>();
+
+        var back = new InlineKeyboardButton();
+        back.setText("Вернуться к репозиторию");
+        back.setCallbackData("BACK_FOR_REPO_MESSAGE");
+
+        rowLine.add(back);
+        rowsLine.add(rowLine);
+
+        markup.setKeyboard(rowsLine);
+        sendMessage.setReplyMarkup(markup);
+
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            log.error("error " + e.getMessage());
+        }
+    }
+
+    public void httpCarSalonApi(long chatId){
+        SendMessage sendMessage = new SendMessage();
+        String text = "http: \n" + "https://github.com/isshepelev/car-salon-api.git";
+        sendMessage.setText(text);
+        sendMessage.setChatId(String.valueOf(chatId));
+
+        InlineKeyboardMarkup markup = new InlineKeyboardMarkup();
+        List<List<InlineKeyboardButton>> rowsLine = new ArrayList<>();
+        List<InlineKeyboardButton> rowLine = new ArrayList<>();
+
+        var back = new InlineKeyboardButton();
+        back.setText("Вернуться к репозиторию");
+        back.setCallbackData("BACK_FOR_REPO_MESSAGE");
+
+        rowLine.add(back);
+        rowsLine.add(rowLine);
+
+        markup.setKeyboard(rowsLine);
+        sendMessage.setReplyMarkup(markup);
+
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            log.error("error " + e.getMessage());
+        }
+    }
+    public void infoCarSalonApi(long chatId){
+        SendMessage sendMessage = new SendMessage();
+        String text = "\uD83D\uDE43 Ой-ой, кажется, тут должно было быть описание к проекту, но мне было лень его делать. \uD83D\uDE05\n" +
+                "\n" +
+                "Если вам действительно интересно, о чем этот проект, то, не стесняйтесь спрашивать.\uD83E\uDD70";
+        sendMessage.setText(text);
+        sendMessage.setChatId(String.valueOf(chatId));
+
+        InlineKeyboardMarkup markup = new InlineKeyboardMarkup();
+        List<List<InlineKeyboardButton>> rowsLine = new ArrayList<>();
+        List<InlineKeyboardButton> rowLine = new ArrayList<>();
+
+        var back = new InlineKeyboardButton();
+        back.setText("Вернуться к репозиторию");
+        back.setCallbackData("BACK_FOR_REPO_MESSAGE");
+
+        rowLine.add(back);
+        rowsLine.add(rowLine);
+
+        markup.setKeyboard(rowsLine);
+        sendMessage.setReplyMarkup(markup);
+
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            log.error("error " + e.getMessage());
+        }
     }
 }
