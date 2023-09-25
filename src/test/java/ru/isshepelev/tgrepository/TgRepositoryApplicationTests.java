5a9aa5af0ea975fd4fcb7c2e2f6db6707a3a1179
Ilya <isshepelev2004@gmail.com>
2023-09-26 00:48:52 +0300

    ref tests

--- src/test/java/ru/isshepelev/tgrepository/TgRepositoryApplicationTests.java
+++ src/test/java/ru/isshepelev/tgrepository/TgRepositoryApplicationTests.java
@@ -3,7 +3,7 @@ package ru.isshepelev.tgrepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class TgRepositoryApplicationTests {
 
     @Test
